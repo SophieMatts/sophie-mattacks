3d8cc5ddbb90adcfbded77bdb707c9a5ff9bfbd2
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-04-24 13:49:18 +0100

    Initial layout complete

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'  >
+      <div className='profile-container'  >
+        <div className='profile-contents-container'  >
+          <div className='name-div'>
+
+          </div>
+          <div className='blurb-div'>
+
+          </div>
+          <div className='link-div'>
+
+          </div>
+
+
+        </div>
+        <div className='profile-image-container'  >
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
