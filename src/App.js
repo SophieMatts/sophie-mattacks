a753b7cc069a8ebc7bd2f6a899f9df149c0049d3
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-04-24 15:38:32 +0100

    Added Icons and more colours

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -7,14 +9,18 @@ function App() {
       <div className='profile-container'  >
         <div className='profile-contents-container'  >
           <div className='name-div'>
-            <h1> I'm Sophie </h1>
-            <h1> A Web Developer! </h1>
+            <div className='first-name-text margin-text'>I'm Sophie</div>
+            <div className='margin-text'> A Web Developer! </div>
           </div>
           <div className='blurb-div'>
-            <h1> BLURB! </h1>
+            <div className='margin-text'> London based developer with experience in: Ruby, Rails, JavaScript and React.  </div>
           </div>
           <div className='link-div'>
-            <h1> LINKTHS! </h1>
+
+            <FontAwesomeIcon className='linkedin' icon={faLinkedin} />
+            <FontAwesomeIcon className='linkedin' icon={faGithub} />
+            <FontAwesomeIcon className='linkedin' icon={faEnvelope} />
+
           </div>
 
 
