f5ca6ea6879d119ba1bf6d6d8f92123e7a1aa9bb
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-05-10 14:38:26 +0100

    Created Pages for Navbar

--- src/index.js
+++ src/index.js
@@ -3,9 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from '../src/Pages/Layout';
+import About from "../src/Pages/About";
+import Home from "../src/Pages/Home";
+import Contact from "../src/Pages/Contact";
+import Projects from "../src/Pages/Projects";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />, document.getElementById('root'));
+
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Home" element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="Contact" element={<Contact />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
