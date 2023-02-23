13b1206827ae966df5ecdb72dfb18594f09d4685
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-02-23 14:02:31 +0000

    About Me Page: Added: Lower banner, things that make me different section, topic boxes with icons.

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -22,20 +22,26 @@ const About = () => {
             I spent the first 10 year stretch of my working life in commercial office furniture and office interiors.
           </div>
           <div className="the-beginning-text-2">
-            It was only then during the Covid-19 pandemic that a friend suggested an app for me to try called "Playgrounds", a game-based coding app, covering the fundamentals of “Swift”.
+            It was only during the Covid-19 pandemic, that a friend suggested an app for me to try called "Playgrounds" - a game-based coding app, covering the fundamentals of “Swift”.
           </div>
           <div className="the-beginning-text-3">
             It was from that moment of playing the game that lead me into wanting to know more about software development.
           </div>
           <div className="the-beginning-text-4">
-            Eventually, I decided to take the plunge and sign up to a web development bootcamp at Le Wagon, to see what it was really all about.
-            Since graduating, I haven't looked back! and continue to devlop my technical skills.
+            Eventually, I decided to take the plunge and sign up to a web development bootcamp at Le Wagon, to see what being a developer was really all about.
           </div>
-
-
-
-
-
+          <div className="the-beginning-text-5">
+            Since graduating, I haven't looked back! and continue to develop my technical skills.
+          </div>
+          <div className="my-vision-header">
+            MY VISION
+          </div>
+          <div className="my-vision-text-1">
+            I want to create and contribute to projects that make a positive difference to people’s lives.
+          </div>
+          <div className="my-vision-text-2">
+            Whether that be a learning tool to help someone understand a difficult concept, or even a fun creation to simply boost your mood and mental health.
+          </div>
         </div>
         <div className="img-box-2">
           <div className="img-lewagon"></div>
@@ -43,39 +49,139 @@ const About = () => {
       </div>
     </div>
 
+    <div className="neurodiversity" >
+      <div className="nd-header">
+        <div className="nd-header-text">
+          The things that make me different...
+        </div>
+      </div>
+    </div>
+
     <div className='neurodiversity-div'>
+      <div className="ndcontent-container">
 
-      <h1>Neurodiversity...</h1>
-      <p>
-        In the summer of 2020, approaching my 30th birthday, I was diagnosed with combined-type ADHD.
-        I’ve always felt different to a lot of people and It explained a lot of things for me. Especially the issues I’ve had in the past with mainstream education and social anxieties.
-        Although ADHD is classed as a disability, there are so many amazing attributes that a lot of people don’t know about.
-      </p>
-      <p>
-        For instance, my planning and organisational skills are exceptional. I’m always thinking far ahead into the future, which allows me make strong decisions and plan for eventualities for further down the line. It also allows me to have different outlooks and approaches to tasks that others might not think of - making me a natural problem solver.
-      </p>
-      <p>
-        I always get a huge dopamine rush from finding/creating helpful solutions. One in particular was creating an ADHD App, called “Clarity”, as part of my Bootcamp final group project.
-      </p>
-      <p>
-        As well as the practical side, I’m very empathic and sensitive to myself and other people.
-        I’m always thinking about how someone else might be feeling and do my best to make them feel valued and supported.
-      </p>
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-solid fa-bolt fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Neurodiverse
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-solid fa-hand-sparkles fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Creative
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-regular fa-lightbulb fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Problem Solver
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-solid fa-people-roof fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Versitile
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-solid fa-person-rays fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Enthusiastic
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="nd-box">
+          <div className="box-content-container">
+            <div className="icon-column">
+              <div className="nd-icon">
+                <i class="fa-regular fa-handshake fa-6x"></i>
+              </div>
+            </div>
+            <div className="side-column">
+              <div className="box-header">
+                Mentorship
+              </div>
+              <div className="box-text">
+                Some information about me etc.
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+      </div>
 
     </div>
 
-    <div className='my-vision-div'>
-      <h1>My Vision...</h1>
-      <p>
-        Going forward, I want to create and contribute to projects that make a positive difference to people’s lives.
-        Whether that be a learning tool to help someone understand a difficult concept, or even a fun creation to simply boost your mood and mental health.
-      </p>
-      <p>
-        As mentioned earlier, I also want to become a mentor for neurodiverse people in tech, especially women.
-        I want to be the person that I never had during my learning journey. Someone who can inspire and get the best out of people not only through the hard times, but through the good times as well.
-      </p>
 
-    </div>
 
 
 
