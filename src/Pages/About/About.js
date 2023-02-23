b9e05d8fd0e1452dd7359b30e8af0af34e9b7e85
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-02-23 17:40:32 +0000

    About Me Page: Added: Topic box content and styling

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -64,7 +64,7 @@ const About = () => {
           <div className="box-content-container">
             <div className="icon-column">
               <div className="nd-icon">
-                <i class="fa-solid fa-bolt fa-6x"></i>
+                <i class="fa-solid fa-bolt fa-7x"></i>
               </div>
             </div>
             <div className="side-column">
@@ -72,7 +72,19 @@ const About = () => {
                 Neurodiverse
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className='p'>
+                  In the summer of 2020, approaching my 30th birthday, I was diagnosed with combined-type ADHD.
+                </div>
+                <div className='p'>
+                  Although ADHD is classed as a disability, there are so many amazing strengths that a lot of people don’t know about.
+                </div>
+                <div className='p'>
+                  I call them superpowers.
+                </div>
+                <div className='p'>
+                  Some of the superpowers I identify with include being: amazing in a crisis, super creative and super intuitive.
+                </div>
+
               </div>
             </div>
           </div>
@@ -90,7 +102,18 @@ const About = () => {
                 Creative
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className="p">
+                  Web design and user experience are both huge a passions of mine.
+                </div>
+                <div className="p">
+                  Being naturally empathetic, I really enjoy thinking about the user's needs when it comes to the design.
+                </div>
+                <div className="p">
+                  As well as being creative on the solution side, I also take a lot of pride in my aestetics skills.
+                </div>
+                <div className="p">
+                  In the end, when an idea all comes together and is executed, I get such a dopamine rush!
+                </div>
               </div>
             </div>
           </div>
@@ -100,7 +123,7 @@ const About = () => {
           <div className="box-content-container">
             <div className="icon-column">
               <div className="nd-icon">
-                <i class="fa-regular fa-lightbulb fa-6x"></i>
+                <i class="fa-regular fa-lightbulb fa-7x"></i>
               </div>
             </div>
             <div className="side-column">
@@ -108,7 +131,15 @@ const About = () => {
                 Problem Solver
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className="p">
+                  Having ADHD allows me to have different outlooks and approaches to tasks that others might not think of - making me a natural problem solver.
+                </div>
+                <div className="p">
+                  When planning projects and preparing code, I always think ahead into the future.
+                </div>
+                <div className="p">
+                  This allows me to make strong decisions and plan for eventualities that may happen further down the line.
+                </div>
               </div>
             </div>
           </div>
@@ -118,7 +149,7 @@ const About = () => {
           <div className="box-content-container">
             <div className="icon-column">
               <div className="nd-icon">
-                <i class="fa-solid fa-people-roof fa-6x"></i>
+                <i class="fa-solid fa-people-roof fa-5x"></i>
               </div>
             </div>
             <div className="side-column">
@@ -126,7 +157,15 @@ const About = () => {
                 Versitile
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className="p">
+                  Since completing my A-Levels, I’ve had a vast variety of jobs to date, including: a makeup artist, a bank cashier and cabin crew - to name a few!
+                </div>
+                <div className="p">
+                  The experience I have working in varied environments has allowed me to develop a wide range of skills.
+                </div>
+                <div className="p">
+                  The most significant are being able to think on my feet, adapt to new situations and the ability to learn quickly.
+                </div>
               </div>
             </div>
           </div>
@@ -144,7 +183,18 @@ const About = () => {
                 Enthusiastic
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className="p">
+                  I'm always the person asking, "but, why?"
+                </div>
+                <div className="p">
+                  and that's because I've always been interested in learning how things work.
+                </div>
+                <div className="p">
+                  I have a strong can-do attitude, and approach everything as a learning experience.
+                </div>
+                <div className="p">
+                  My journey with tech so far has been incredibly eye-opening, and I'm really keen to learn more and develop my skills.
+                </div>
               </div>
             </div>
           </div>
@@ -154,7 +204,7 @@ const About = () => {
           <div className="box-content-container">
             <div className="icon-column">
               <div className="nd-icon">
-                <i class="fa-regular fa-handshake fa-6x"></i>
+                <i class="fa-regular fa-handshake fa-5x"></i>
               </div>
             </div>
             <div className="side-column">
@@ -162,7 +212,15 @@ const About = () => {
                 Mentorship
               </div>
               <div className="box-text">
-                Some information about me etc.
+                <div className="p">
+                  I always wonder how different things would be if I had the right support when I needed it most.
+                </div>
+                <div className="p">
+                  That's why I'm striving to become a mentor for neurodiverse people in tech, especially women.
+                </div>
+                <div className="p">
+                  Not only to inspire and get the best out of people during the hard times, but celebrating all of the wins - no matter what the size!
+                </div>
               </div>
             </div>
           </div>
