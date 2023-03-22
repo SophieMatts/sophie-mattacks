b6d238288617f3f0d745dddd6295a3db0d67ef97
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-03-22 16:06:22 +0000

    Formatted contact page after submit message button pressed.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,7 +5,7 @@ import './ContactForm.css'
 const FORM_ENDPOINT = "https://public.herotofu.com/v1/2eb8f710-c723-11ed-b4ee-9d74f7714d9e";
 
 const ContactForm = () => {
- const [submitted, setSubmitted] = useState(true);
+ const [submitted, setSubmitted] = useState(false);
   const handleSubmit = e => {
     setTimeout(() => {
       setSubmitted(true);
@@ -42,15 +42,41 @@ const ContactForm = () => {
 
   if (submitted) {
     return (
-      <>
-        <div className="contact-container">
-          <h2>Thank you!</h2>
-          <p>We'll be in touch soon.</p>
-          <Link to="/">Home</Link>
-        </div>
-      </>
+
+      <div className="contact-form-container">
+        <form className="form"
+        action={FORM_ENDPOINT}
+          onSubmit={handleSubmit}
+          method="POST"
+        >
+          <div className="form-title">
+            Let's get in touch...
+          </div>
+
+          <div className="form-inputs-container">
+            <div className="name-box">
+              <input type="text" placeholder="Your name" name="name" required />
+            </div>
+            <div className="email-box">
+              <input type="email" placeholder="Email" name="email" required />
+            </div>
+            <div className="message-box">
+              <textarea placeholder="Your message" name="message" required />
+            </div>
+            <div className="submit-box">
+              <button type="submit"> Send a message </button>
+            </div>
+          </div>
+
+          <div className="message-sent">
+            <p>Message sent!</p>
+          </div>
+
+        </form>
+      </div>
+
     );
-  }
+  };
 
   return (
     <div className="contact-form-container">
